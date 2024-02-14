13ece48bf1282c708b6f635fb1b81427bedbe90d
felix <felix@plan-immobilier.fr>
2024-02-14 12:10:49 +0100

    modif controler

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,17 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'app_conference')]
+//    #[Route('/conference', name: 'app_conference')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-//        return $this->render('conference/index.html.twig', [
-//            'controller_name' => 'ConferenceController',
-//        ]);
         return new Response(<<<EOF
            <html>
                 <body>
