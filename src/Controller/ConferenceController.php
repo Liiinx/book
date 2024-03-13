0c58000fdc50427628b7b419b34a57d17bd75151
felix <felix@plan-immobilier.fr>
2024-03-13 16:13:45 +0100

    message when comment conference, email to user when comment accepted

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,7 +9,8 @@ use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
@@ -65,6 +66,7 @@ class ConferenceController extends AbstractController
     public function show(Request $request,
                          Conference $conference,
                          CommentRepository $commentRepository,
+                         NotifierInterface $notifier,
 //                         SpamChecker $spamChecker,
                          #[Autowire('%photo_dir%')] string $photoDir): Response
     {
@@ -97,9 +99,15 @@ class ConferenceController extends AbstractController
             // message dans le bus. Le gestionnaire décide alors ce qu'il en fait.
             $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
+            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
+
             return $this->redirectToRoute('conference', [
                 'slug' => $conference->getSlug()
             ]);
+        }
+
+        if ($form->isSubmitted()) {
+            $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
         }
 
         $offset = max(0, $request->query->getInt('offset', 0));
